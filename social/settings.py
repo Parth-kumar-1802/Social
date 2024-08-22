d337a24f17415de6d152552608537f7faaf53cfa
Parth-kumar-1802 <parthkumar1802@gmail.com>
2024-08-22 23:37:06 +0530

    host

--- social/settings.py
+++ social/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,12 +78,16 @@ WSGI_APPLICATION = 'social.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL","postgresql://socialdb_wtwj_user:mLrr7w9hhhuIqnr4Y7dYKArdSXFHgbbM@dpg-cr3nhd2j1k6c73dqu6ag-a.singapore-postgres.render.com/socialdb_wtwj"))
 }
+os.getenv("DATABASE_URL")
 
 
 # Password validation
